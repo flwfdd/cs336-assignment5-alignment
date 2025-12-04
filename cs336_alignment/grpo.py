25a95a1729058c34a11f3fdca5f4c3c0c687daa1
flwfdd <flwfdd@qq.com>
2025-12-04 06:04:08 +0000

    feat: compute_grpo_clip_loss

--- cs336_alignment/grpo.py
+++ cs336_alignment/grpo.py
@@ -73,3 +73,31 @@ def compute_naive_policy_gradient_loss(
         be aggregated across the batch and sequence dimensions in the training loop).
     """
     return -policy_log_probs * raw_rewards_or_advantages
+
+
+def compute_grpo_clip_loss(
+    advantages: torch.Tensor,
+    policy_log_probs: torch.Tensor,
+    old_log_probs: torch.Tensor,
+    cliprange: float,
+) -> tuple[torch.Tensor, dict[str, torch.Tensor]]:
+    """
+    Args:
+    advantages: torch.Tensor Shape (batch_size, 1), per-example advantages A.
+    policy_log_probs: torch.Tensor Shape (batch_size, sequence_length), per-token log
+        probs from the policy being trained.
+    old_log_probs: torch.Tensor Shape (batch_size, sequence_length), per-token log probs
+        from the old policy.
+    cliprange: float Clip parameter ϵ (e.g. 0.2).
+
+    Returns:
+    tuple[torch.Tensor, dict[str, torch.Tensor]].
+    loss: torch.Tensor of shape (batch_size, sequence_length), the per-token clipped
+        loss.
+    metadata: dict containing whatever you want to log. We suggest logging whether each
+        token was clipped or not, i.e., whether the clipped policy gradient loss on the RHS of
+        the min was lower than the LHS.
+    """
+    ratio = torch.exp(policy_log_probs - old_log_probs)
+    clip_ratio = torch.clamp(ratio, 1.0 - cliprange, 1.0 + cliprange)
+    return -torch.min(ratio * advantages, clip_ratio * advantages), {}
